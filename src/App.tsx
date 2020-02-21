def54d2fa52f555cbf3e02bf23f4e87a28263a96
Rahul Parmar <rahulparmar.n@gmail.com>
2020-02-21 07:44:03 +0530

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Login } from './components/Login'
+import { Login } from './components/Login/Login'
+import { Home } from './components/Home/Home'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
-    </div>
+
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
